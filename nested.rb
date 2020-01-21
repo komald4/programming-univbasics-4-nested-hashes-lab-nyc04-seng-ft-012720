2903d005db11dfca22371e72675a4a504383e770
calm-fork-7535 <komald.4@gmail.com>
2020-01-21 22:41:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,9 +82,9 @@ def changing_alan
 	}
 }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-		 alans_new_info = "GUI"
-		#programmer_hash[grace_hopper][:known_for] = alans_new_info
-	   #programmer_hash
+		alans_new_info = "GUI"
+		programmer_hash[grace_hopper][:known_for] = alans_new_info
+	  programmer_hash
 end
 
 def adding_to_dennis
