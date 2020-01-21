5977e252e8e7d92bca335423a420075a4000069e
calm-fork-7535 <komald.4@gmail.com>
2020-01-21 22:48:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,12 +2,12 @@
 def hopper
 	programmer_hash =
  		{
-        :grace_hopper => {
+        #:grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         }
-     }
-programmer_hash[:grace_hopper]
+
+
 end
 
 def alan_kay_is_known_for
