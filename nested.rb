43c7515ad5b2edda7b23eb1aa4a8c1b3d2e292a4
calm-fork-7535 <komald.4@gmail.com>
2020-01-21 22:27:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,14 +81,11 @@ def changing_alan
 		#:languages => ["C"]
 	}
 }
-end
-  alans_new_info = "GUI"
-  programmer_hash[:grace_hopper][:known_for] = alans_new_info
-  programmer_hash
-
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-
-
+		 alans_new_info = "GUI"
+		 programmer_hash[:grace_hopper][:known_for] = alans_new_info
+		 programmer_hash
+end
 
 def adding_to_dennis
 	programmer_hash =
