7df7a84a84512a72a313b9205d5297557d4121a9
calm-fork-7535 <komald.4@gmail.com>
2020-01-21 22:25:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,6 +80,7 @@ def changing_alan
 		:known_for => "COBOL"
 		#:languages => ["C"]
 	}
+}
   alans_new_info = "GUI"
   programmer_hash[:grace_hopper][:known_for] = alans_new_info
   programmer_hash
