11cf33626a57a6beaa2452b6fa4876516f214ec1
calm-fork-7535 <komald.4@gmail.com>
2020-01-21 22:25:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ def changing_alan
   alans_new_info = "GUI"
   programmer_hash[:grace_hopper][:known_for] = alans_new_info
   programmer_hash
-}
+
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
 
 end
